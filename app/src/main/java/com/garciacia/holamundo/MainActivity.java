d087512322575a59200c3c483e0199e0d91dae5d
garciacia <t00044507@utbvirtual.edu.co>
2018-12-15 22:00:01 -0500

    Cualquier pendejada

---
+++ app/src/main/java/com/garciacia/holamundo/MainActivity.java
@@ -0,0 +1,32 @@
+package com.garciacia.holamundo;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+        }
+
+        public void cambiartexto(View view) {
+            TextView Texto = (TextView) findViewById(R.id.Texto);
+            Texto.setText("Hola mi amor");
+        }
+
+        public void cambiarcolor(View view) {
+            TextView Texto = (TextView) findViewById(R.id.Texto);
+            Texto.setTextColor(Color.MAGENTA);
+        }
+
+        public void restaurar(View view) {
+            TextView Texto = (TextView) findViewById(R.id.Texto);
+            Texto.setText("Hola mundo");
+            Texto.setTextColor(Color.GRAY);
+        }
+    }
